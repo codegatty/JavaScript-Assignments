2a77a6e0ccbaf062cbbf6291b0d9a724409dcc9c
codegatty <rajithgatty61@gmail.com>
2026-02-14 22:51:23 +0530

    --updated medium

--- 01-async-js/medium/1-file-cleaner.js
+++ 01-async-js/medium/1-file-cleaner.js
@@ -11,3 +11,37 @@
 // ```
 // hello world my name is raman
 // ```
+
+const fs = require("fs");
+function promisifiedReadFile(fileName, encoding) {
+  console.log("Reading the file");
+  return new Promise((resolve, reject) => {
+    fs.readFile(fileName, encoding, (err, data) => {
+      if (err) reject(err);
+
+      resolve(data);
+    });
+  });
+}
+
+function promisifiedWriteFile(fileName, data) {
+  console.log("Writing the file");
+  return new Promise((resolve, reject) => {
+    fs.writeFile(fileName, data, (err) => {
+      if (err) reject(err);
+      resolve("File has been written successfully");
+    });
+  });
+}
+
+async function main() {
+  const fileName = "file.txt";
+  const encoding = "utf-8";
+  const data = await promisifiedReadFile(fileName, encoding);
+  const cleanedString = data.split(/\s+/).join(' '); //\s - whitespace \n \t etc 
+  await promisifiedWriteFile(fileName, cleanedString);
+  console.log("Program completed");
+}
+
+console.log("Program started");
+main();
--- 01-async-js/medium/2-clock.js
+++ 01-async-js/medium/2-clock.js
@@ -1,8 +1,41 @@
 // Using `1-counter.md` or `2-counter.md` from the easy section, can you create a
 // clock that shows you the current machine time?
 
-// Can you make it so that it updates every second, and shows time in the following formats - 
+// Can you make it so that it updates every second, and shows time in the following formats -
 
 //  - HH:MM::SS (Eg. 13:45:23)
 
 //  - HH:MM::SS AM/PM (Eg 01:45:23 PM)
+
+function timeFormatter24(hour, min, sec) {
+  console.log(`${String(hour).padStart(2,'0')}:${String(min).padStart(2,'0')}:${String(sec).padStart(2,'0')}`);
+}
+
+function timeFormatter12(hour, min, sec) {
+  let meridiem = hour >= 12 ? "PM" : "AM";
+
+  let hour12 = hour % 12;
+  if (hour12 === 0) hour12 = 12;
+
+  console.log(`${String(hour12).padStart(2,'0')}:${String(min).padStart(2,'0')}:${String(sec).padStart(2,'0')} ${meridiem}`);
+}
+
+function interval() {
+  let now = new Date();
+
+  timeFormatter24(
+    now.getHours(),
+    now.getMinutes(),
+    now.getSeconds()
+  );
+
+  timeFormatter12(
+    now.getHours(),
+    now.getMinutes(),
+    now.getSeconds()
+  );
+
+  setTimeout(interval, 1000);
+}
+
+interval();
