87c1aadb333cbc391f0129c260c23a102c3ea87f
codegatty <rajithgatty61@gmail.com>
2026-03-01 05:38:10 +0530

    --updated

--- 01-js/easy/expenditure-analysis.js
+++ 01-js/easy/expenditure-analysis.js
@@ -8,8 +8,65 @@
   - `npm run test-expenditure-analysis`
 */
 
+// const transactions = [
+//   {
+//     itemName: "Milk",
+//     category: "Groceries",
+//     price: 60,
+//     timestamp: 1708502400000,
+//   },
+//   {
+//     itemName: "Bread",
+//     category: "Groceries",
+//     price: 40,
+//     timestamp: 1708506000000,
+//   },
+//   {
+//     itemName: "Movie Ticket",
+//     category: "Entertainment",
+//     price: 250,
+//     timestamp: 1708510000000,
+//   },
+//   {
+//     itemName: "Bus Ticket",
+//     category: "Transport",
+//     price: 30,
+//     timestamp: 1708515000000,
+//   },
+//   {
+//     itemName: "Petrol",
+//     category: "Transport",
+//     price: 500,
+//     timestamp: 1708520000000,
+//   },
+//   {
+//     itemName: "Netflix Subscription",
+//     category: "Entertainment",
+//     price: 499,
+//     timestamp: 1708525000000,
+//   },
+// ];
+
 function calculateTotalSpentByCategory(transactions) {
-    return [];
+  const finder = [];
+  const result = [];
+  transactions.forEach((element) => {
+    const category = element.category;
+    if (finder.includes(category)) {
+      const index = finder.indexOf(category);
+      const oldData = result[index];
+      const newSpentTotal = oldData.totalSpent + element.price;
+      result[index] = { category: category, totalSpent: newSpentTotal };
+    } else {
+      finder.push(element.category);
+      result.push({ category: category, totalSpent: element.price });
+    }
+  });
+
+  return result;
 }
 
+//console.log(calculateTotalSpentByCategory(transactions));
+
+//console.log(calculateTotalSpentByCategory(transactions));
 module.exports = calculateTotalSpentByCategory;
